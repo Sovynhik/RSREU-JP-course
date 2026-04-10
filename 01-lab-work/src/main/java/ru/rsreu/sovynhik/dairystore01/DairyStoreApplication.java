3e5668988daba759537a26fa4f305e00315dab42
Daniel Savushkin <savushckindaniil@yandex.ru>
2026-04-10 18:35:14 +0300

    feat(lab-01): add base logic

---
+++ 01-lab-work/src/main/java/ru/rsreu/sovynhik/dairystore01/DairyStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.rsreu.sovynhik.dairystore01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairyStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DairyStoreApplication.class, args);
+    }
+
+}
