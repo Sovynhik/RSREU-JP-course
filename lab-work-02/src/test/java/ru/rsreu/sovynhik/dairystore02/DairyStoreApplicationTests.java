3226c85ad994bb39047ebe03e16013e7dbb6aaf4
Daniel Savushkin <savushckindaniil@yandex.ru>
2026-04-10 19:44:30 +0300

    chore(core): setup application.yaml

---
+++ lab-work-02/src/test/java/ru/rsreu/sovynhik/dairystore02/DairyStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rsreu.sovynhik.dairystore02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
