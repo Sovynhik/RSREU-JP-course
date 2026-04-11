1613f7523a04644c27729c2076dd56c4f55918b7
Daniel Savushkin <savushckindaniil@yandex.ru>
2026-04-11 03:17:17 +0300

    feat(lab-04): add base structure new module

---
+++ lab-work-04/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.5"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "ru.rsreu.sovynhik"
+version = "0.0.1-SNAPSHOT"
+description = "lab-work-04"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ lab-work-04/src/test/java/ru/rsreu/sovynhik/dairystore04/DairyStoreApplication4Tests.java
@@ -0,0 +1,13 @@
+package ru.rsreu.sovynhik.dairystore04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyStoreApplication4Tests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
